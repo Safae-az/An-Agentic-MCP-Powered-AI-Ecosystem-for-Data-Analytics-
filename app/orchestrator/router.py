c517690ee2eda00194b6f9dfee35a7ee434920ee
oumaimasahli37 <oumaymasahli37@gmail.com>
2026-04-23 19:40:03 +0100

    Update router.py

--- app/orchestrator/router.py
+++ app/orchestrator/router.py
@@ -1,40 +1,25 @@
-# # Associe chaque étape du pipeline à son agent
-# STEP_TO_AGENT = {
-#     "data_quality":   "data_engineer",
-#     "data_engineer":  "data_engineer",
-#     "kpi_calculator": "data_scientist",
-#     "bi_agent":       "bi_agent",
-#     "reporter":       "reporter",
-# }
-
-
-# class Router:
-#     """
-#     Reçoit le nom d'une étape et retourne
-#     le nom de l'agent qui doit l'exécuter.
-#     """
-
-#     def pick_agent(self, step: str) -> str:
-#         agent = STEP_TO_AGENT.get(step)
-#         if not agent:
-#             raise ValueError(f"Aucun agent trouvé pour l'étape '{step}'")
-#         return agent
-
 
 # app/orchestrator/router.py
-from app.agents.data_engineer import DataEngineerAgent
+from app.agents.data_engineer  import DataEngineerAgent
 from app.agents.data_scientist import DataScientistAgent
 
 
 class Router:
-    def get_agent(self, task_name: str, run_id: str):
+    """
+    Décide QUI fait la tâche.
+    Reçoit un nom de tâche et retourne le bon agent instancié.
+    """
 
-        if task_name == 'data_engineer':
+    def get_agent(self, task_name: str, run_id: str):
+        print(f"[Router] Tache '{task_name}' → agent correspondant")
+
+        if task_name == "data_engineer":
             return DataEngineerAgent(run_id=run_id)
 
-        if task_name == 'data_scientist':
-            return DataScientistAgent(run_id=run_id)  # ← AJOUTER
+        elif task_name == "data_scientist":
+            return DataScientistAgent(run_id=run_id)
 
-        print(f"[Router] '{task_name}' pas encore disponible")
-        return None
-
+        # bi_agent et reporter seront ajoutés par P3 et P4
+        else:
+            print(f"[Router] Agent '{task_name}' pas encore disponible — skip")
+            return None
